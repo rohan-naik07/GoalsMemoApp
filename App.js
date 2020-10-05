c172472552564cb7bf6d1254cb3bb63426dad5cd
Rohan Naik <royalronny12@gmail.com>
2020-10-05 17:30:42 +0530

    Added all files

--- App.js
+++ App.js
@@ -1,21 +1,43 @@
 import { StatusBar } from 'expo-status-bar';
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import React,{useState} from 'react';
+import { StyleSheet, Text, View, Button ,TextInput , ScrollView,FlatList} from 'react-native';
+import GoalItem from './components/GoalItem';
+import GoalInput from './components/GoalInput';
 
 export default function App() {
+  const [courseGoals,setCourseGoals] = useState([]);
+  const [InputOpen,toggleInput] = useState(false);
+
+  const addGoalHandler = (enteredGoal)=>{
+    console.log(enteredGoal);
+    setCourseGoals(currentGoals=>[
+      ...currentGoals,
+      {key : enteredGoal,value : enteredGoal}]
+      )
+      toggleInput(false);
+    }
+  const removeGoalHandler = (goalKey)=>{
+    setCourseGoals(currentGoals=> currentGoals.filter((goal)=>goal.key !== goalKey));
+  }
+  const onGoalCanceled = ()=>{
+    toggleInput(false);
+  }
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+      <View style={styles.container}>
+        <Button title = "Add new Goal" onPress={()=>toggleInput(true)}/>
+        <GoalInput visible = {InputOpen} onAddGoal = {addGoalHandler} onCancel={onGoalCanceled}/>
+        <FlatList 
+        keyExtractor = {(item,index)=>item.key}
+        data = {courseGoals} 
+          renderItem={itemData=><GoalItem id={itemData.item.key} 
+          onDelete = {removeGoalHandler.bind(itemData.item.key)} title = {itemData.item.value}/>}/>
+      </View> 
   );
 }
 
 const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
+ container : {
+   padding : 30,
+   justifyContent : "center"
+ }
 });
